ebb1c4337a1cb66beb1f3fcd94b5e9ba4033e6c6
varun4 <varun09054@gmail.com>
2013-11-18 17:12:54 +0530

    Fixed forecast

--- columbiaweather.py
+++ columbiaweather.py
@@ -3,14 +3,16 @@ from datetime import datetime
 
 
 def forecast(lat,lon):
-    day_of_year = datetime.now().timetuple().tm_yday
+    day_of_year = str(datetime.now().timetuple().tm_yday)
     done = False
-    forecastURL = "http://iridl.ldeo.columbia.edu/expert/home/.benno/.IMD/.three-hourly/.INDIA/.APCP/L/0.0625/0.9375/RANGE/S/"+day_of_year+"/VALUE/lat/"+lat+"/VALUE/lon/"+lon+"/VALUE/L/SUM/data.tsv"
+    forecastURL = "http://iridl.ldeo.columbia.edu/expert/home/.benno/.IMD/.three-hourly/.INDIA/.APCP/L/0.0625/0.9375/RANGE/S/"+day_of_year+"/VALUE/lat/"+str(lat)+"/VALUE/lon/"+str(lon)+"/VALUE/L/SUM/data.tsv"
     while(not done):
         try:
+            ##print forecastURL
             result = urlopen(forecastURL)
+            data = result.read()
             done = True
         except:
             done = False
     
-    return result
+    return data
